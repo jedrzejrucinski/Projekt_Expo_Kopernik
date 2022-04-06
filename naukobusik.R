cc26e267d07d757f2230d0dd6d9e0eb71fa967f2
grzegorzpioro <72441347+grzegorzpioro@users.noreply.github.com>
2022-04-06 18:23:41 +0200

    Update naukobusik.R

--- naukobusik.R
+++ naukobusik.R
@@ -2,15 +2,18 @@ library(dplyr)
 library(ggplot2)
 library(readr)
 library(tidyr)
+library(forcats)
+library(stringi)
+library(stringr)
 kopernik <- read_csv("kopernik.csv")
 kopernik <- kopernik %>% 
   distinct(id_ucznia, miejsc, .keep_all = TRUE)
-
+colnames(kopernik) <- stri_replace_all_fixed(colnames(kopernik), "P ", "P")
 
 kopernik <- kopernik %>% 
   select(!weryfikator:woj_nr)
-kopernik %>% 
-  filter_all(any_vars(. %in% c(99,'99')))
+kopernik <- kopernik %>% 
+  stri_replace_all_regex("P ","P")
 
 data <- data.frame(columns = colnames(kopernik), ile_99 = rep(0, 195))
 
@@ -22,9 +25,16 @@ for (i in iter){
     filter(col1 == 99) %>% 
     count()
 }
-data %>% 
-  filter(ile_99 <2000) %>% 
-  ggplot(aes(x = columns, y = ile_99))+
-  geom_col()
+
+g <- data %>% 
+  filter(ile_99 <= quantile(ile_99,probs = 0.96), row_number() <= n()-1) %>% 
+  filter(!str_detect(columns, "P45")) %>% 
+  mutate(ile_99 = 100*ile_99/dim(kopernik)[1]) %>% 
+  ggplot(aes(x = forcats::fct_inorder(factor(columns)), y = ile_99, group = columns))+
+  geom_col(fill = 'royalblue1', color = 'royalblue1')
+
+g + labs(title = "Procent braków danych w pytaniach w ankiecie Naukobus", x = 'Pytania', y = 'Procent braków danych(%)')
+
+
 
 
