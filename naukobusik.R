dfe59e3835fc36c38f1a0c7a39b053cdc3a9f6d2
grzegorzpioro <72441347+grzegorzpioro@users.noreply.github.com>
2022-04-07 15:09:36 +0200

    Update naukobusik.R

--- naukobusik.R
+++ naukobusik.R
@@ -5,36 +5,45 @@ library(tidyr)
 library(forcats)
 library(stringi)
 library(stringr)
-kopernik <- read_csv("kopernik.csv")
+kopernik <- read_csv("Projekt_eksplo/kopernik.csv")
 kopernik <- kopernik %>% 
   distinct(id_ucznia, miejsc, .keep_all = TRUE)
 colnames(kopernik) <- stri_replace_all_fixed(colnames(kopernik), "P ", "P")
 
 kopernik <- kopernik %>% 
-  select(!weryfikator:woj_nr)
-kopernik <- kopernik %>% 
-  stri_replace_all_regex("P ","P")
+  select(!weryfikator:woj_nr) %>% 
+  select(!P45:P45k)
 
-data <- data.frame(columns = colnames(kopernik), ile_99 = rep(0, 195))
+data_99 <- data.frame(columns = colnames(kopernik), ile_99 = rep(0, dim(kopernik)[2]))
 
 iter <- 1:dim(kopernik)[2]
 for (i in iter){
-  data[i,2] <- kopernik %>% 
+  data_99[i,2] <- kopernik %>% 
     select(i) %>% 
     rename(col1 = 1) %>% 
     filter(col1 == 99) %>% 
     count()
 }
+subs <- data_99 %>% 
+  filter(ile_99 <= quantile(ile_99,probs = 0.96), row_number() <= n()-1) %>%
+  mutate(ile_99 = 100*ile_99/dim(kopernik)[1]) %>%
+  mutate("prev_value" = lag(ile_99)) %>%
+  filter(ile_99-prev_value > 1.5) %>% 
+  select(columns,ile_99)
 
-g <- data %>% 
+
+
+
+g <- data_99 %>% 
   filter(ile_99 <= quantile(ile_99,probs = 0.96), row_number() <= n()-1) %>% 
   filter(!str_detect(columns, "P45")) %>% 
   mutate(ile_99 = 100*ile_99/dim(kopernik)[1]) %>% 
-  ggplot(aes(x = forcats::fct_inorder(factor(columns)), y = ile_99, group = columns))+
-  geom_col(fill = 'royalblue1', color = 'royalblue1')
+  ggplot(aes(x = forcats::fct_inorder(factor(columns)), y = ile_99))+
+  geom_col(fill = 'royalblue1', color = 'royalblue1')+
+  geom_text(data = subs, aes(label = columns), size = 3,nudge_y = 0.5)
 
-g + labs(title = "Procent braków danych w pytaniach w ankiecie Naukobus", x = 'Pytania', y = 'Procent braków danych(%)')
-
-
+g + labs(title = "Procent braków danych w pytaniach w ankiecie Naukobus", x = 'Pytania', y = 'Procent braków danych(%)')+
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
 
 
